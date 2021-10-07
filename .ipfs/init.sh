0872a5f086ba36e4101d47983febf69cc13f6b5a
jacky.li <jacky.li@accenture.com>
2021-10-07 18:49:21 +0800

    update for doc

--- .ipfs/init.sh
+++
@@ -1,6 +0,0 @@
-ipfs config --json Experimental.Libp2pStreamMounting true
-ipfs config --json Swarm.EnableRelayHop true
-
-ipfs bootstrap add /ip4/139.198.14.205/tcp/4001/p2p/12D3KooWQHFNeGSnCCAWiDefPDw7xBdQbiRwAUQ5zoQixCr4dgWJ
-ipfs bootstrap add /ip4/139.198.4.185/tcp/4001/p2p/12D3KooWS7KaWoaZpsNRcozjQpYptx4VH4sH5kUSTTJNYzcrW9DE
-
